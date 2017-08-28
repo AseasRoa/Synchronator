38d48cd2fbb51bd237074d16b4bda879ef05e4c5
AseasRoa <aseasroa@yahoo.com>
2017-08-28 23:39:12 +0300

    example-transform
    example-transform

---
+++ test/example-transform.js
@@ -0,0 +1,22 @@
+require("synchronator")
+
+var code = `
+var fn1 = function*(time)
+{
+    var result = sleep(time)
+
+    return result
+})
+
+var fn2 = function*()
+{
+    var result = fn1(1000)
+
+    return result
+})`
+
+var transformed_code = Synchronator.transform(code)
+
+// "transformed_code" is now an object and we can find the code in .code
+
+console.log(transformed_code.code)
